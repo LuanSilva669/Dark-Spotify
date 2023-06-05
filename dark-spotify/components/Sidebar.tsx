3642aa055084b5c91b9ac659e5e43e662409d6df
LuanSilva669 <125572605+LuanSilva669@users.noreply.github.com>
2023-06-05 19:47:13 -0300

    a

---
+++ dark-spotify/components/Sidebar.tsx
@@ -0,0 +1,15 @@
+interface SidebarProps {
+    children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({
+    children 
+}) => {
+return (
+  <div>
+    oi
+  </div>
+);
+}
+
+export default Sidebar;
